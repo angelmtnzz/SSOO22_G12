d2e73b27e6204d884adabf29015f7cee45e14471
angelmtnzz <amartf29@estudiantes.unileon.es>
2022-12-23 18:53:05 +0100

    Creados programa.c y script.sh

---
+++ programa.c
@@ -0,0 +1,17 @@
+// Practica Final SSOO_2022 G12
+//
+// Integrantes:	Ángel Martínez Fernández (C)
+// 				Saad Ali Hussain Kausar
+// 				Javier Álvarez Pintor
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (int argc, char *argv[]){
+
+}
